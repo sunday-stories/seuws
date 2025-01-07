02047aa4ba4fca65ac61c13f2f7f609d918a38a2
blackarmourr <abhijithjithuba@gmail.com>
2025-01-07 10:38:40 +0530

    trail

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
                 <button onClick={() => navigate("/appliance-management")}>Manage Appliances</button>
                 <button onClick={() => navigate("/inverter-details")}>Inverter Details</button>
                 <button onClick={() => navigate("/billing")}>Billing</button>
-                <button onClick={() => navigate("/report")}>Generate Report</button> {/* New Report Button */}
+                <button onClick={() => navigate("/report")}>Energy Report</button> {/* New Report Button */}
             </div>
 
             <div className="energy-consumption">
